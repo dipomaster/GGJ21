e642846b6443d5870c08401fff882c46b95d1c81
dMaster <mstrdipo@gmail.com>
2021-01-28 16:43:10 +0000

    Wip Menu

--- Unityproj/GGJ1_Lost&Found/Assets/Resources/Scripts/Menu_Item.cs
+++ Unityproj/GGJ1_Lost&Found/Assets/Resources/Scripts/Menu_Item.cs
@@ -5,22 +5,21 @@ public class Menu_Item : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField] private Button PrevButton;
     [SerializeField] private Button NextButton;
-
+    [SerializeField] public GameObject[] items;
     private int currentItem;
 
-private void Awake() {
-    SelectMenuItem(0);
-}
-
+    private void Awake () {
+        SelectMenuItem (0);
+        
+    }
 
     private void SelectMenuItem (int _index) {
 
-PrevButton.interactable=(_index!=0);
-NextButton.interactable=(_index!=transform.childCount-1);
+        PrevButton.interactable = (_index != 0);
+        NextButton.interactable = (_index != transform.childCount - 1);
         for (int i = 0; i < transform.childCount; i++) {
             transform.GetChild (i).gameObject.SetActive (i == _index);
         }
-
     }
 
     public void ChangeItem (int _change) {
@@ -28,5 +27,5 @@ NextButton.interactable=(_index!=transform.childCount-1);
         SelectMenuItem (currentItem);
 
     }
-    
+
 }
