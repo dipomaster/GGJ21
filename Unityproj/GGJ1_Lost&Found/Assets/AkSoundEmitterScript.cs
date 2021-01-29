dd71e3cca66d49cad6bd67ead557fa666eb4130e
ASpaceCadet-Audio <wgpbiggs@gmail.com>
2021-01-29 22:00:10 +0000

    Added Wwise Switch Functionality

--- Unityproj/GGJ1_Lost&Found/Assets/AkSoundEmitterScript.cs
+++ Unityproj/GGJ1_Lost&Found/Assets/AkSoundEmitterScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,47 @@ public class AkSoundEmitterScript : MonoBehaviour
 {
     public AK.Wwise.Event StartEvent;
     public AK.Wwise.Event StopEvent;
+    public AK.Wwise.Switch CharacterSelect;
+    public AK.Wwise.Switch EmotionSelect;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartEvent.Post(gameObject);
+
+    }
+
+    private void Update()
+    {
+        SelectCharacter();
+        SelectEmotion();
+        IsTalking();
+    }
+
+    private void SelectEmotion()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EmotionSelect.SetValue(gameObject);
+        }
+    }
+
+    private void SelectCharacter()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CharacterSelect.SetValue(gameObject);
+        }
+    }
+
+    private void IsTalking()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartEvent.Post(gameObject);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopEvent.Post(gameObject);
+        }
     }
 }
