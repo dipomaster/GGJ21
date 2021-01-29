416d483b1067afa0064bdfbc9c998557c4797bbe
ASpaceCadet-Audio <wgpbiggs@gmail.com>
2021-01-29 13:38:09 +0000

    Wwise Integrated

---
+++ Unityproj/GGJ1_Lost&Found/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID STOP_TALKING = 2173668446U;
+        static const AkUniqueID TALK = 3173192383U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace CHARACTER
+        {
+            static const AkUniqueID GROUP = 436743010U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GOTH = 786076171U;
+                static const AkUniqueID MAGIC_GIRL = 594300751U;
+            } // namespace SWITCH
+        } // namespace CHARACTER
+
+        namespace EMOTION
+        {
+            static const AkUniqueID GROUP = 3146275604U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ANGRY = 1206605712U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace SWITCH
+        } // namespace EMOTION
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
