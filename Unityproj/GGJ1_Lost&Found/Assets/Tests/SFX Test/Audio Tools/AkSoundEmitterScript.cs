215e883c5365da2fa1a2efd986343051e6bb7ec1
ASpaceCadet-Audio <wgpbiggs@gmail.com>
2021-01-30 15:28:03 +0000

    Wwise integration with Fungus

--- Unityproj/GGJ1_Lost&Found/Assets/Tests/SFX Test/Audio Tools/AkSoundEmitterScript.cs
+++ Unityproj/GGJ1_Lost&Found/Assets/Tests/SFX Test/Audio Tools/AkSoundEmitterScript.cs
@@ -7,38 +7,13 @@ public class AkSoundEmitterScript : MonoBehaviour
 {
     public AK.Wwise.Event StartTalking;
     public AK.Wwise.Event StopTalking;
-    public AK.Wwise.Switch[] SelectCharacter;
+    public AK.Wwise.Switch SelectCharacter;
     public AK.Wwise.Switch[] SelectEmotion;
 
     private void Update()
     {
-        ManageCharacter();
+        SelectCharacter.SetValue(gameObject);
         ManageEmotion();
-        ManageSpeech();
-    }
-
-    private void ManageCharacter()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SelectCharacter[0].SetValue(gameObject);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SelectCharacter[1].SetValue(gameObject);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            SelectCharacter[2].SetValue(gameObject);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            SelectCharacter[3].SetValue(gameObject);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            SelectCharacter[4].SetValue(gameObject);
-        }
     }
 
     private void ManageEmotion()
@@ -61,15 +36,13 @@ public class AkSoundEmitterScript : MonoBehaviour
         }
     }
 
-    private void ManageSpeech()
+    public void StartSpeech()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartTalking.Post(gameObject);
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            StopTalking.Post(gameObject);
-        }
+        StartTalking.Post(gameObject);
+    }
+
+    public void StopSpeech()
+    {
+        StopTalking.Post(gameObject);
     }
 }
