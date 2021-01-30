b05d56c2d3604503c292271db9f6795c2d8762d0
ASpaceCadet-Audio <wgpbiggs@gmail.com>
2021-01-30 12:25:38 +0000

    Added Nervous Voices

--- Unityproj/GGJ1_Lost&Found/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unityproj/GGJ1_Lost&Found/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -41,6 +41,7 @@ namespace AK
             namespace SWITCH
             {
                 static const AkUniqueID ANGRY = 1206605712U;
+                static const AkUniqueID NERVOUS = 1717373301U;
                 static const AkUniqueID NEUTRAL = 670611050U;
             } // namespace SWITCH
         } // namespace EMOTION
